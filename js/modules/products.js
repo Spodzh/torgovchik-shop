23198133b3533204f6d66e459bc88c7e0b8590cb
Spodzh <164197243+Spodzh@users.noreply.github.com>
2026-07-13 15:03:59 +0300

    Create products.js

---
+++ js/modules/products.js
@@ -0,0 +1,507 @@
+// ===== ТОВАРЫ =====
+export const products = [
+    // -------- ЖИДКОСТИ (category: 'liquids') --------
+    // Rick and Morty Bad Acid (4)
+    {
+        id: 1,
+        name: 'Ананасовый леденец',
+        brand: 'Rick and Morty Bad Acid',
+        price: 15,
+        image: 'https://i.ibb.co/Pv414S0f/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 2,
+        name: 'Киви Лайм',
+        brand: 'Rick and Morty Bad Acid',
+        price: 15,
+        image: 'https://i.ibb.co/wr4btGb7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 3,
+        name: 'Клюква Лайм',
+        brand: 'Rick and Morty Bad Acid',
+        price: 15,
+        image: 'https://i.ibb.co/M5sYqrWy/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 4,
+        name: 'Вишня Клюква',
+        brand: 'Rick and Morty Bad Acid',
+        price: 15,
+        image: 'https://i.ibb.co/vrtTs55/image.png',
+        category: 'liquids'
+    },
+
+    // Подонки Подгон (3)
+    {
+        id: 5,
+        name: 'Апельсин мята',
+        brand: 'Подонки Подгон',
+        price: 15,
+        image: 'https://i.ibb.co/Kpnn3QMB/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 6,
+        name: 'Апельсин',
+        brand: 'Подонки Подгон',
+        price: 15,
+        image: 'https://i.ibb.co/yFg8TvMd/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 7,
+        name: 'Малиновая конфета',
+        brand: 'Подонки Подгон',
+        price: 15,
+        image: 'https://i.ibb.co/BkQGJzy/image.png',
+        category: 'liquids'
+    },
+
+    // Подонки инферно (3)
+    {
+        id: 8,
+        name: 'Клубника грейпфрут малина',
+        brand: 'Подонки инферно',
+        price: 17,
+        image: 'https://i.ibb.co/nMdy9kWx/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 9,
+        name: 'Клубничная шипучка',
+        brand: 'Подонки инферно',
+        price: 17,
+        image: 'https://i.ibb.co/b5d7KM0L/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 10,
+        name: 'Клубника банан',
+        brand: 'Подонки инферно',
+        price: 17,
+        image: 'https://i.ibb.co/LXjMvDCC/image.png',
+        category: 'liquids'
+    },
+
+    // Podonki Blood (12)
+    {
+        id: 11,
+        name: 'Черная смородина',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 12,
+        name: 'Классический бабл гам',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 13,
+        name: 'Яблоко виноград',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 14,
+        name: 'Персиковый лимонад',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 15,
+        name: 'Скитлс',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 16,
+        name: 'Сочная малина',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 17,
+        name: 'Виноград черника',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 18,
+        name: 'Конфеты смородина мята',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 19,
+        name: 'Брусничный морс',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 20,
+        name: 'Кислые вишневые червячки',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 21,
+        name: 'Банан клубника',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 22,
+        name: 'Клубника ежевика',
+        brand: 'Podonki Blood',
+        price: 15,
+        image: 'https://i.ibb.co/gbcNGhK0/image.png',
+        category: 'liquids'
+    },
+
+    // Annima Love Zombi (18)
+    {
+        id: 23,
+        name: 'Алоэ виноград',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 24,
+        name: 'Киви клубника',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 25,
+        name: 'Персиковый шейк',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 26,
+        name: 'Морозный рэдбул',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 27,
+        name: 'Арбузный бабл гам',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 28,
+        name: 'Клубничный леденец',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 29,
+        name: 'Ананас киви',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 30,
+        name: 'Малина с кислинкой',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 31,
+        name: 'Фанта маракуйя',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 32,
+        name: 'Лесной микс',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 33,
+        name: 'Мандариновая фанта',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 34,
+        name: 'Вишня лед',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 35,
+        name: 'Ежевичный лимонад',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 36,
+        name: 'Яблоко персик',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 37,
+        name: 'Клубнично-вишневый лимонад',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 38,
+        name: 'Клубника банан',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 39,
+        name: 'Яблоко виноград',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 40,
+        name: 'Энергетик виноград',
+        brand: 'Annima Love Zombi',
+        price: 17,
+        image: 'https://i.ibb.co/wFfDBkK7/image.png',
+        category: 'liquids'
+    },
+
+    // Zloy Hard (10)
+    {
+        id: 41,
+        name: 'Морс из лесных ягод',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 42,
+        name: 'Мандарин персик',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 43,
+        name: 'Клюква апельсин',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 44,
+        name: 'Виноград арбуз',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 45,
+        name: 'Клубника банан',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 46,
+        name: 'Фруктовый мармелад',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 47,
+        name: 'Виноградный чупа-чупс',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 48,
+        name: 'Мята спрайт',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 49,
+        name: 'Персик абрикос',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 50,
+        name: 'Малиновая газировка',
+        brand: 'Zloy Hard',
+        price: 17,
+        image: 'https://i.ibb.co/zHHYpfYR/image.png',
+        category: 'liquids'
+    },
+
+    // Rick and Morty & catswill (6)
+    {
+        id: 51,
+        name: 'Арбуз морозная черешня',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/0R4TwBF3/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 52,
+        name: 'Малина яблоко лед',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/mjssmH8/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 53,
+        name: 'Банан дыня мята',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/Z64thsqD/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 54,
+        name: 'Кислые ленточки клубника ежевика',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/My9syDsC/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 55,
+        name: 'Фрутелла лайм лимон малина',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/fVNgQ30K/image.png',
+        category: 'liquids'
+    },
+    {
+        id: 56,
+        name: 'Голубика арбуз лед',
+        brand: 'Rick and Morty & catswill',
+        price: 18,
+        image: 'https://i.ibb.co/KjqwSZkD/image.png',
+        category: 'liquids'
+    },
+
+    // -------- ИСПАРИТЕЛИ / КАРТРИДЖИ (category: 'coils') --------
+    {
+        id: 57,
+        name: 'Испар Хиро 50-58 W, 0.2 Ом',
+        brand: 'Hiro',
+        price: 12,
+        image: 'https://i.ibb.co/XMCFVwD/image.png',
+        category: 'coils'
+    },
+    {
+        id: 58,
+        name: 'Испар 0.3 Ом, 50-65 W',
+        brand: 'Испаритель',
+        price: 12,
+        image: 'https://i.ibb.co/21YMfYVD/image.png',
+        category: 'coils'
+    },
+    {
+        id: 59,
+        name: 'VooPoo 0.7 Ом',
+        brand: 'VooPoo',
+        price: 12,
+        image: 'https://i.ibb.co/DPrMb7P5/image.png',
+        category: 'coils'
+    },
+    {
+        id: 60,
+        name: 'Картридж 0.4 Ом',
+        brand: 'Картридж',
+        price: 12,
+        image: 'https://i.ibb.co/6R04Z3st/image.png',
+        category: 'coils'
+    },
+    {
+        id: 61,
+        name: 'Картридж 0.6 Ом',
+        brand: 'Картридж',
+        price: 12,
+        image: 'https://i.ibb.co/MD9X53XD/image.png',
+        category: 'coils'
+    }
+];
